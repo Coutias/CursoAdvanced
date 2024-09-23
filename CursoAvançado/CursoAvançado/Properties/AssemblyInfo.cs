71fec953536ec7c527358ae8e8818f07814db1be
Coutias <tiagopuridadegoncalves@gmail.com>
2024-09-23 20:01:57 -0300

    Sistema de Carregamento
    Foi completo o sistema de carregamento inicial.
    + Formulário de log-in (incompleto)
    Aula02

---
+++ CursoAvançado/CursoAvançado/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("CursoAvançado")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("8454f5a4sf652asf")]
+[assembly: AssemblyProduct("CursoAvançado")]
+[assembly: AssemblyCopyright("Copyright © 8454f5a4sf652asf 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("fbb6686c-8251-4afd-93d6-c89699d6fe40")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
